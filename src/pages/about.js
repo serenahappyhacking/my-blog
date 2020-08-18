677c34b3daa0ec2fb399149af2ba38195a6b2068
Serena Wang <serena.happyhacking@gmail.com>
2020-08-18 18:12:59 +0800

    second commit

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,14 @@
 import React from "react"
+import Layout from "../components/layout"
 
-const AboutPage = () => (
-  <main>
-    <h1> Serena's Blog</h1>
-    <p> Welcome to my gatsby site !</p>
-  </main>
-)
-
-export default AboutPage
+export default function About() {
+  return (
+    <Layout>
+      <h1>About Pandas Eating Lots</h1>
+      <p>
+        We're the only site running on your computer dedicated to showing the
+        best photos and videos of pandas eating lots of food.
+      </p>
+    </Layout>
+  )
+}
